359c24fc4509d669d2487cf9a8c2caae7a252ea4
Steven Fawcett <steve.fawcett@capgemini.com>
2020-07-09 16:02:48 +0100

    Stripped white space

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,9 @@
 
 while getopts m:f: o
 do	case "$o" in
-	m)	mode="$OPTARG" ;;
+	m)	mode=$(echo "$OPTARG" | tr -d '[:space:]') ;;
 	f)	file="$OPTARG" ;;
 	[?])	exit 1;;
 	esac
 done
-
 ruby a11y-crawl.rb "--"${mode} ${file} -U ${HTTP_USERNAME} -P ${HTTP_PASSWORD}
