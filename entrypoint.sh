e20fc37216209b00fc6fba824caa2684618b9fa2
Steven Fawcett <steve.fawcett@capgemini.com>
2020-07-10 10:15:56 +0100

    Entrypoint amended

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,11 @@ mode=${1}
 file=${2}
 
 case ${mode} in
-     sitemap) ;;
-     file);;
-     *) exit(1);;
+     sitemap) echo "Processing Sitemap" ;;
+     file) echo "Processing File based" ;;
+     *) echo "Unknown option"
+        return 1 ;;
 esac
 
 CMD=$(find . -name a11y-crawl.rb)
-echo ruby ${CMD} "--${mode}" "${file}" -U ${HTTP_USERNAME} -P ${HTTP_PASSWORD}
+ruby ${CMD} "--${mode}" "${file}" -U ${HTTP_USERNAME} -P ${HTTP_PASSWORD}
